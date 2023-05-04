bb27f39b4f6aa809836c871ea2702126a6bc0868
CodeBustler <codebustler360online@gmail.com>
2023-05-04 13:22:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# darkModeToggle
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-25ayrw)
---
+++ script.js
@@ -0,0 +1,8 @@
+let navBar = document.querySelector('#navbar');
+let darkMode = document.querySelector('#dark-mode-icon');
+let body = document.body;
+
+darkMode.addEventListener('click', () => {
+  navBar.classList.toggle('dark-nav');
+  body.classList.toggle('dark-mode');
+});
